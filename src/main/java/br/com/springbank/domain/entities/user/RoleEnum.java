cf20f2b240b86fc33d10a7ba42d3862d1de3a388
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-13 11:08:50 -0300

    feat: cria os enums para as roles e status do usuario

---
+++ src/main/java/br/com/springbank/domain/entities/user/RoleEnum.java
@@ -0,0 +1,6 @@
+package br.com.springbank.domain.entities.user;
+
+public enum RoleEnum {
+    USER,
+    ADMIN;
+}
---
+++ src/main/java/br/com/springbank/domain/entities/user/StatusEnum.java
@@ -0,0 +1,6 @@
+package br.com.springbank.domain.entities.user;
+
+public enum StatusEnum {
+    ACTIVE,
+    INACTIVE;
+}

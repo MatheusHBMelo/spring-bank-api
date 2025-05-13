c98ac0fff2b1a8a2aebe5a90b663aa8ba003dead
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-13 11:07:27 -0300

    feat: cria a entidade de usuario e seus atributos

---
+++ src/main/java/br/com/springbank/domain/entities/user/UserEntity.java
@@ -0,0 +1,42 @@
+package br.com.springbank.domain.entities.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(of = "id")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    private String name;
+
+    @JoinColumn(nullable = false, unique = true)
+    private String email;
+
+    @JoinColumn(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "tb_user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<RoleEntity> role = new HashSet<>();
+
+    @Enumerated(EnumType.STRING)
+    private StatusEnum status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}

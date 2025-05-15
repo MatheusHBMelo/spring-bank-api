38cb271aafcb1a6df301f2272afd25078d2e48c6
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-15 11:25:30 -0300

    feat: cria o endpoint de registro de usuario

---
+++ src/main/java/br/com/springbank/controller/auth/AuthController.java
@@ -0,0 +1,26 @@
+package br.com.springbank.controller.auth;
+
+import br.com.springbank.controller.auth.dto.RegisterDto;
+import br.com.springbank.service.user.UserDetailsServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth")
+public class AuthController {
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public AuthController(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<Void> register(@RequestBody RegisterDto registerDto) {
+        this.userDetailsService.registerUser(registerDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}

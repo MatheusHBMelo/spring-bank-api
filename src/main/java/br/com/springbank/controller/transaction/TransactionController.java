7c56386de0d39b6a86ccf8f4f7b44a0c4722d741
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-20 10:17:50 -0300

    feat: cria o controllador de transacoes e adiciona o endpoint para transacao entre contas

---
+++ src/main/java/br/com/springbank/controller/transaction/TransactionController.java
@@ -0,0 +1,26 @@
+package br.com.springbank.controller.transaction;
+
+import br.com.springbank.controller.transaction.dto.TransferRequestDto;
+import br.com.springbank.service.transaction.TransactionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/transaction")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping(path = "/transfer")
+    public ResponseEntity<Void> transfer(@RequestBody TransferRequestDto transferRequestDto){
+        this.transactionService.transfer(transferRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}

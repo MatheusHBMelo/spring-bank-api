74e03b1f41c9e68f2406d0a4306724105629d113
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-21 21:14:05 -0300

    feat: cria o endpoint para extrato bancario

--- src/main/java/br/com/springbank/controller/transaction/TransactionController.java
+++ src/main/java/br/com/springbank/controller/transaction/TransactionController.java
@@ -1,15 +1,15 @@
 package br.com.springbank.controller.transaction;
 
 import br.com.springbank.controller.transaction.dto.DepositRequestDto;
+import br.com.springbank.controller.transaction.dto.StatementResponseDto;
 import br.com.springbank.controller.transaction.dto.TransferRequestDto;
 import br.com.springbank.controller.transaction.dto.WithdrawRequestDto;
 import br.com.springbank.service.transaction.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/transaction")
@@ -21,20 +21,25 @@ public class TransactionController {
     }
 
     @PostMapping(path = "/transfer")
-    public ResponseEntity<Void> transfer(@RequestBody TransferRequestDto transferRequestDto){
+    public ResponseEntity<Void> transfer(@RequestBody TransferRequestDto transferRequestDto) {
         this.transactionService.transfer(transferRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping(path = "/deposit")
-    public ResponseEntity<Void> transfer(@RequestBody DepositRequestDto depositRequestDto){
+    public ResponseEntity<Void> transfer(@RequestBody DepositRequestDto depositRequestDto) {
         this.transactionService.deposit(depositRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping(path = "/withdraw")
-    public ResponseEntity<Void> withdraw(@RequestBody WithdrawRequestDto withdrawRequestDto){
+    public ResponseEntity<Void> withdraw(@RequestBody WithdrawRequestDto withdrawRequestDto) {
         this.transactionService.withdraw(withdrawRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping(path = "/statement")
+    public ResponseEntity<List<StatementResponseDto>> bankStatement() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.transactionService.bankStatement());
     }
 }

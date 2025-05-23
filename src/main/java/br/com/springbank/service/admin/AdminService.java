ee2ea334ba10797329321562fe407eb76ca4aa91
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-23 11:37:10 -0300

    feat: adiciona funcionalidade administrativa que retorna um usuario dado seu username

--- src/main/java/br/com/springbank/service/admin/AdminService.java
+++ src/main/java/br/com/springbank/service/admin/AdminService.java
@@ -1,5 +1,6 @@
 package br.com.springbank.service.admin;
 
+import br.com.springbank.controller.admin.dto.UserRequestDto;
 import br.com.springbank.controller.admin.dto.UsersResponseDto;
 import br.com.springbank.domain.entities.user.UserEntity;
 import br.com.springbank.domain.repositories.account.AccountRepository;
@@ -25,5 +26,11 @@ public class AdminService {
         List<UserEntity> users = this.userRepository.findAll();
 
         return users.stream().map(UsersResponseDto::fromUserEntity).toList();
+    }
+
+    public UsersResponseDto findUserByUsername(UserRequestDto userRequestDto) {
+        UserEntity user = this.userRepository.findByUsername(userRequestDto.username()).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+
+        return UsersResponseDto.fromUserEntity(user);
     }
 }

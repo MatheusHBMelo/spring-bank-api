25a4c27a695fa9a2eaccc87f69c432aafc4af445
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-23 19:11:02 -0300

    feat: habilita o endpoint para retornar todas as accounts

--- src/main/java/br/com/springbank/controller/admin/AdminController.java
+++ src/main/java/br/com/springbank/controller/admin/AdminController.java
@@ -1,5 +1,6 @@
 package br.com.springbank.controller.admin;
 
+import br.com.springbank.controller.admin.dto.AccountResponseDto;
 import br.com.springbank.controller.admin.dto.TransactionsResponseDto;
 import br.com.springbank.controller.admin.dto.UserRequestDto;
 import br.com.springbank.controller.admin.dto.UsersResponseDto;
@@ -38,5 +39,10 @@ public class AdminController {
     @GetMapping(path = "/transactions")
     public ResponseEntity<List<TransactionsResponseDto>> findAllTransactions() {
         return ResponseEntity.status(HttpStatus.OK).body(this.adminService.findAllTransactions());
+    }
+
+    @GetMapping(path = "/accounts")
+    public ResponseEntity<List<AccountResponseDto>> findAllAccounts() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.adminService.findAllAccounts());
     }
 }

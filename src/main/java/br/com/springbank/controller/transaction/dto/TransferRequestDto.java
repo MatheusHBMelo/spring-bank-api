684a6b8ebedf8e276c2b3b450e6aeb3941891dd8
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-23 10:33:02 -0300

    feat: adiciona validacao para o campos de entrada das operacoes financeiras

--- src/main/java/br/com/springbank/controller/transaction/dto/TransferRequestDto.java
+++ src/main/java/br/com/springbank/controller/transaction/dto/TransferRequestDto.java
@@ -1,6 +1,14 @@
 package br.com.springbank.controller.transaction.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record TransferRequestDto(BigDecimal amount, String receiverAccountNumber) {
+public record TransferRequestDto(@NotNull(message = "O campo amount não pode ser nulo")
+                                 @DecimalMin(value = "0.00", inclusive = false, message = "O campo amount deve ser maior que 0.00")
+                                 @Digits(integer = 19, fraction = 2) BigDecimal amount,
+                                 @NotBlank(message = "O campo do destinatario de transferencia não pode estar em branco") String receiverAccountNumber) {
 }
--- src/main/java/br/com/springbank/controller/transaction/dto/WithdrawRequestDto.java
+++ src/main/java/br/com/springbank/controller/transaction/dto/WithdrawRequestDto.java
@@ -1,6 +1,12 @@
 package br.com.springbank.controller.transaction.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record WithdrawRequestDto(BigDecimal amount) {
+public record WithdrawRequestDto(@NotNull(message = "O campo amount não pode ser nulo")
+                                 @DecimalMin(value = "0.00", inclusive = false, message = "O campo amount deve ser maior que 0.00")
+                                 @Digits(integer = 19, fraction = 2) BigDecimal amount) {
 }

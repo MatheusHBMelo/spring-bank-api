3cf5cd776b532d2d60be3333238f7a3bf743c477
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-23 19:11:48 -0300

    feat: habilita o endpoing, administrativo, que retorna todas as accounts

--- src/main/java/br/com/springbank/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/springbank/config/security/SecurityConfiguration.java
@@ -51,6 +51,7 @@ public class SecurityConfiguration {
                     request.requestMatchers(HttpMethod.GET, "/admin/user").hasAuthority("ROLE_ADMIN");
                     request.requestMatchers(HttpMethod.PATCH, "/admin/user").hasAuthority("ROLE_ADMIN");
                     request.requestMatchers(HttpMethod.GET, "/admin/transactions").hasAuthority("ROLE_ADMIN");
+                    request.requestMatchers(HttpMethod.GET, "/admin/accounts").hasAuthority("ROLE_ADMIN");
                     request.anyRequest().denyAll();
                 })
                 .addFilterBefore(tokenFilter, BasicAuthenticationFilter.class)

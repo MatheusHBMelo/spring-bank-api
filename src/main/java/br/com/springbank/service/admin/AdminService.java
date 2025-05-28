9f214f16c0c74ff2229abbe720b8c5433f366924
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-28 11:10:39 -0300

    feat: adiciona validacoes e evita duplicidade de funcoes

--- src/main/java/br/com/springbank/service/admin/AdminService.java
+++ src/main/java/br/com/springbank/service/admin/AdminService.java
@@ -1,6 +1,8 @@
 package br.com.springbank.service.admin;
 
-import br.com.springbank.controller.admin.dto.*;
+import br.com.springbank.controller.admin.dto.AccountResponseDto;
+import br.com.springbank.controller.admin.dto.TransactionsResponseDto;
+import br.com.springbank.controller.admin.dto.UsersResponseDto;
 import br.com.springbank.domain.entities.account.AccountEntity;
 import br.com.springbank.domain.entities.account.TransactionEntity;
 import br.com.springbank.domain.entities.user.StatusEnum;
@@ -31,15 +33,28 @@ public class AdminService {
         return users.stream().map(UsersResponseDto::fromUserEntity).toList();
     }
 
-    public UsersResponseDto findUserByUsername(UserRequestDto userRequestDto) {
-        UserEntity user = this.userRepository.findByUsername(userRequestDto.username()).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+    public UsersResponseDto findUserByUsername(String username) {
+        if (username == null || username.trim().isEmpty()) {
+            throw new RuntimeException("O username é obrigatório.");
+        }
+
+        UserEntity user = this.userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Este usuario não foi encontrado."));
 
         return UsersResponseDto.fromUserEntity(user);
     }
 
     @Transactional
-    public UsersResponseDto disableUser(UserRequestDto userRequestDto){
-        UserEntity user = this.userRepository.findByUsername(userRequestDto.username()).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+    public UsersResponseDto disableUser(String username) {
+        if (username == null || username.trim().isEmpty()) {
+            throw new RuntimeException("O username é obrigatório.");
+        }
+
+        UserEntity user = this.userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Este usuario não foi encontrado."));
+
+        if (user.getStatus().equals(StatusEnum.INACTIVE)) {
+            throw new RuntimeException("Este usuário já está desativado.");
+        }
+
         user.setStatus(StatusEnum.INACTIVE);
 
         this.userRepository.save(user);
@@ -59,9 +74,13 @@ public class AdminService {
         return accounts.stream().map(AccountResponseDto::fromAccountEntity).toList();
     }
 
-    public AccountResponseDto findAccountByAccountNumber(NumberAccountRequestDto numberAccountRequestDto) {
-        AccountEntity account = this.accountRepository.findByAccountNumber(numberAccountRequestDto.accountNumber())
-                .orElseThrow(() -> new RuntimeException("Conta não encontrada"));
+    public AccountResponseDto findAccountByAccountNumber(String numberAccount) {
+        if (numberAccount == null || numberAccount.trim().isEmpty()) {
+            throw new RuntimeException("O número da conta é obrigatório.");
+        }
+
+        AccountEntity account = this.accountRepository.findByAccountNumber(numberAccount)
+                .orElseThrow(() -> new RuntimeException("Esta conta não foi encontrada."));
 
         return AccountResponseDto.fromAccountEntity(account);
     }

2f381083c8f92f828538d595a00c88bae7729f99
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-28 11:11:58 -0300

    refactor: melhora o formatador de datas evitando nova instanciacao a cada chamada

--- src/main/java/br/com/springbank/utils/DatetimeFormatter.java
+++ src/main/java/br/com/springbank/utils/DatetimeFormatter.java
@@ -4,8 +4,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DatetimeFormatter {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
     public static String formatDateTime(LocalDateTime dateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        return dateTime.format(formatter);
+        return dateTime.format(FORMATTER);
     }
 }

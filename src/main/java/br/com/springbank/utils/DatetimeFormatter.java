0756b7e6a74f24289bb54f00121cf4fa99c3d1b5
MatheusHBMelo <matheushbmelo@gmail.com>
2025-05-29 11:15:28 -0300

    feat: converte essa classe para uma utilityclass do lombok

--- src/main/java/br/com/springbank/utils/DatetimeFormatter.java
+++ src/main/java/br/com/springbank/utils/DatetimeFormatter.java
@@ -1,8 +1,11 @@
 package br.com.springbank.utils;
 
+import lombok.experimental.UtilityClass;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@UtilityClass
 public class DatetimeFormatter {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 

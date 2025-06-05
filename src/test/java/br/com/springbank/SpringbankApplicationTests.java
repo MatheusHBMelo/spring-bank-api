3a2257dcf14ed6ea8781de05e954b49aeb69f03e
MatheusHBMelo <matheushbmelo@gmail.com>
2025-06-05 11:49:14 -0300

    feat: configura a propriedade de teste

--- src/test/java/br/com/springbank/SpringbankApplicationTests.java
+++ src/test/java/br/com/springbank/SpringbankApplicationTests.java
@@ -2,12 +2,14 @@ package br.com.springbank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringbankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
